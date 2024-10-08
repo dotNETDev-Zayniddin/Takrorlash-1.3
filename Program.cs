02193fb5805a8ed2a94bbebd83bf14437ff9d4b9
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-09 02:14:23 +0500

    Takrorlash: Task Submission

--- Program.cs
+++ Program.cs
@@ -11,13 +11,35 @@ namespace Takrorlash
         static void Main()
         {
             System.Console.Write("INT tipidagi son kiriting: ");
+
             int x = Convert.ToInt32(Console.ReadLine());
+            bool isContinue = false;
             long result = 1;
-            for(int i = 1; i <= x; i++)
-            {
-                result *= i;
-            }
-            System.Console.WriteLine("Natija: "  + result);
-        }
+
+            do{
+                
+                if( x > 0 )
+                { 
+                    for(int i = 1; i <= x; i++)
+                    {
+                        result *= i;
+                        isContinue = false;
+                    }
+                    System.Console.WriteLine("Natija: "  + result);
+                }
+                else if( x == 0)
+                {
+                        result = 1;
+                        isContinue = false;
+                        System.Console.WriteLine("Natija: "  + result);
+                }
+                else {
+                    System.Console.Write("Ma'lumot kiritishdagi xatolik. Qayta urunib ko'ring: ");
+                    isContinue = true;
+                    x =Convert.ToInt32(Console.ReadLine());
+                } 
+            }while(isContinue);
+
+            }   
     }
 }
