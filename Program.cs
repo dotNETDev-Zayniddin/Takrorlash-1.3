657a520002795c9bdb1f13a80f3eff93f08205df
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-09 01:56:45 +0500

    Takrorlash: Task Submission

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Consolga int tipidagi butun son kiritilgan => int x
+x! (x factorial) ni hisoblaydigan dastur tuzing.
+
+*/
+using System;
+using System.Collections.Concurrent;
+namespace Takrorlash
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.Write("INT tipidagi son kiriting: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            long result = 1;
+            for(int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+
+            System.Console.WriteLine("Natija: "  + result);
+        }
+    }
+}
